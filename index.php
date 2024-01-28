9cbfad0a9b793cdf540940c01dbe5e0de502cd61
EkaSukra <113070438+EkaSukra@users.noreply.github.com>
2024-01-28 12:05:58 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>FORM INPUT DATA</h1>
+    <form action="proses.php" method="post">
+        <label for="nama">NAMA : </label>
+        <input type="text" name="nama" required><br>
+        <label for="email">EMAIL : </label>
+        <input type="text" name="email" required><br>
+
+        <input type="submit" value="simpan">
+    </form>
+</body>
+</html>
